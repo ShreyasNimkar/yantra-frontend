2f0261c999454b2ab4d2fe9e6abec4bff192e65e
ShreyasNimkar <nimkarshreyas22@gmail.com>
2024-04-14 02:16:40 +0530

    feat:journal icons temp

---
+++ src/sections/Groups/newGroup.tsx
@@ -0,0 +1,17 @@
+import { Group } from "@/types";
+import React from "react";
+
+interface Props {
+  setGroups: React.Dispatch<React.SetStateAction<Group[]>>;
+}
+const NewGroup = ({ setGroups }: Props) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default NewGroup;
